5e0781cd2a173bc3abadfa88d037bd2a0f898f67
zhangyilin <zhangyilin@meetstarinc.com>
2026-08-19 19:03:55 +0800

    feat: publish agentduel-component 0.1.2

--- src/AgentDuelBadgeGallery.tsx
+++ src/AgentDuelBadgeGallery.tsx
@@ -328,9 +328,8 @@ export function AgentDuelOwnedBadgeGallery({
 
 function PencilIcon() {
   return (
-    <svg aria-hidden="true" focusable="false" viewBox="0 0 24 24">
-      <path d="M4 20h4l11-11-4-4L4 16v4Z" />
-      <path d="m14 6 4 4" />
+    <svg aria-hidden="true" fill="none" focusable="false" viewBox="0 0 24 24">
+      <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497zM15 5l4 4" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" />
     </svg>
   );
 }
